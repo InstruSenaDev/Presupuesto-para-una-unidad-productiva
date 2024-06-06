d5ce2c79ae123ed7cd02ca9e3f9b94c20693cafc
Juan Jose <cespedesvillotaj@gmail.com>
2024-06-06 09:51:57 -0500

    arreglo de astro y tailwind
    uwu

--- Frontend/nasty-nadir/src/env.d.ts
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-/// <reference types="astro/client" />
-=======
-/// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client" />
->>>>>>> b9691623f9b13a7da3505566e6a031b58031df76
