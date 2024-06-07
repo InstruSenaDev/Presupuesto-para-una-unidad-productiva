c74675bdd384433871df83a6f857de5f3eb37b8a
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-06-07 11:55:59 -0500

    Vista
    Recuperar contraseña

--- frontEnd1/tailwind.config.mjs
+++ frontEnd1/tailwind.config.mjs
@@ -2,6 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+
+		colors:{
+			'color1':"#D9D9D9",
+			'color2':"#000000",
+			'color3':"#FFFFFF",
+		},
 		extend: {},
 	},
 	plugins: [],
