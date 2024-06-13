d0d23a20a0fa7b9527a58ca59c564b22539179ed
Carlos <cespedesvillotaj@gmail.com>
2024-06-13 11:48:09 -0500

    SQL
    Agregando tablas

--- Backend/controller.js
+++ Backend/controller.js
@@ -1,6 +1,16 @@
 const pool = require("./db2")
 
 
+
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
+    
+    app.post('/update', function(req, res) {
+      const { name, description } = req.body;
+      res.send(`Name ${name}, desc ${description}`);
+    });
+
+
 module.exports = {
 
     
