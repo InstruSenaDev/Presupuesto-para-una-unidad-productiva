c5c7c2dde3cc773ad2cdb188e6841911398ca9f5
Juan Jose <cespedesvillotaj@gmail.com>
2024-06-20 10:23:25 -0500

    .
    .

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+			'blueUwu' : '#322FA0',
+			'blanquito' : '#FFFFFF',
+			'rosadito': '#9747FF',
+		},
 		extend: {},
 	},
 	plugins: [],
