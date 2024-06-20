7ef4a666187bc70042258eada6e39119a32bdd68
Juan Jose <160735061+Andresfelipec20@users.noreply.github.com>
2024-06-20 11:53:36 -0500

    dashboard
    continuacion de diseño

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -2,6 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+
+			'Morado': '#322FA0',
+			'blanco': '#FFFFFF'
+		},
 		extend: {},
 	},
 	plugins: [],
