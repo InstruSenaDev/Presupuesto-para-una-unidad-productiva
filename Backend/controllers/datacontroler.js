15265f6f160121c41233d1298d36223847db36b8
Marcao22 <160734930+Marcao22@users.noreply.github.com>
2024-07-12 11:43:16 -0500

    conexion a bd supabase
    en proceso

---
+++ Backend/controllers/datacontroler.js
@@ -0,0 +1,35 @@
+// Backend/controllers/datacontroller.js
+import { pool } from '../config/db.js';
+
+// Función para obtener todas las personas
+async function getAllPersonas() {
+    try {
+        console.log('Obteniendo todas las personas...');
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM usuarios');
+        client.release();
+        console.log('Personas obtenidas con éxito:', result.rows);
+        return result.rows;
+    } catch (error) {
+        console.error('Error al obtener personas:', error);
+        throw error;
+    }
+}
+
+// Función para obtener todos los usuarios
+async function getAllUsuario() {
+    try {
+        console.log('Obteniendo todos los usuarios...');
+        const client = await pool.connect();
+        const result = await client.query('SELECT * FROM usuarios');
+        client.release();
+        console.log('Usuarios obtenidos con éxito:', result.rows);
+        return result.rows;
+    } catch (error) {
+        console.error('Error al obtener usuarios:', error);
+        throw error;
+    }
+}
+
+
+export { getAllPersonas, getAllUsuario };
---
+++ Backend/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const app = express();
+
+app.listen (8000, function(){
+    console.log("servidor creado http://localhost:8000");
+});
